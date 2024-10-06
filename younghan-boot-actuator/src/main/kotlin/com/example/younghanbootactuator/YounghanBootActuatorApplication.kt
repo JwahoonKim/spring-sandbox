fa8d28e93e9c0bfd7db06fc1ae6dba94eea25b4c
김좌훈 <kjhoon0330@toss.im>
2024-10-06 20:37:54 +0900

    김영한 스프링부트 모니터링 강의 모듈 생성

---
+++ younghan-boot-actuator/src/main/kotlin/com/example/younghanbootactuator/YounghanBootActuatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.younghanbootactuator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YounghanBootActuatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<YounghanBootActuatorApplication>(*args)
+}
---
+++ younghan-boot-actuator/src/test/kotlin/com/example/younghanbootactuator/YounghanBootActuatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.younghanbootactuator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YounghanBootActuatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
